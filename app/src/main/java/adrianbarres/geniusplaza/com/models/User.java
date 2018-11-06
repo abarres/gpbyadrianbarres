ddccc68770a7bb97ad64b087ee8696a138c1ce3e
Adrian Barres <adrian@latingrub.com>
2018-11-05 20:32:31 -0600

    Model creation.

---
+++ app/src/main/java/adrianbarres/geniusplaza/com/models/User.java
@@ -0,0 +1,66 @@
+package adrianbarres.geniusplaza.com.models;
+
+import java.util.List;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class User {
+
+    @SerializedName("page")
+    @Expose
+    private Integer page;
+    @SerializedName("per_page")
+    @Expose
+    private Integer perPage;
+    @SerializedName("total")
+    @Expose
+    private Integer total;
+    @SerializedName("total_pages")
+    @Expose
+    private Integer totalPages;
+
+    @SerializedName("data")
+    @Expose
+    private List<UserData> userData = null;
+
+    public Integer getPage() {
+        return page;
+    }
+
+    public void setPage(Integer page) {
+        this.page = page;
+    }
+
+    public Integer getPerPage() {
+        return perPage;
+    }
+
+    public void setPerPage(Integer perPage) {
+        this.perPage = perPage;
+    }
+
+    public Integer getTotal() {
+        return total;
+    }
+
+    public void setTotal(Integer total) {
+        this.total = total;
+    }
+
+    public Integer getTotalPages() {
+        return totalPages;
+    }
+
+    public void setTotalPages(Integer totalPages) {
+        this.totalPages = totalPages;
+    }
+
+    public List<UserData> getUserData() {
+        return userData;
+    }
+
+    public void setUserData(List<UserData> userData) {
+        this.userData = userData;
+    }
+
+}
